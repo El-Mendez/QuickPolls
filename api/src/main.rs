c209fcb64836cccf42a8a68a836bc0b65fdf6086
Méndez <47764656+El-Mendez@users.noreply.github.com>
2024-06-01 16:54:07 -0600

    add environment variables and logs

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,33 +2,52 @@ mod models;
 mod state;
 mod api;
 
+use std::env;
 use axum::Router;
+use log::info;
+use tracing::Level;
 use tower_http::cors::{CorsLayer, Any};
 use tower_http::services::{ServeDir, ServeFile};
 use socketioxide::SocketIo;
+use tower_http::trace;
+use tower_http::trace::TraceLayer;
 use crate::api::api_routes;
 use crate::state::AppState;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
-    let (layer, io) = SocketIo::new_layer();
-    let state = AppState::new(io).await.expect("could not connect to db");
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .init();
+
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("3000"));
+    let db_uri = env::var("DATABASE_URI").expect("Missing database URI");
+    let run_migrations = env::args().find(|x| x == "--run-migrations").is_some();
+
+    let (socket_io_layer, io) = SocketIo::new_layer();
     let serve_static_files = ServeDir::new("static")
         .not_found_service(ServeFile::new("static/index.html"));
-
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_methods(Any)
         .allow_headers(Any);
+    let logging_layer = TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO));
 
+    let state = AppState::new(io, &db_uri, run_migrations).await.expect("could not connect to db");
     let app = Router::new()
         .nest_service("/api", api_routes(state))
         .fallback_service(serve_static_files)
-        .layer(layer)
+        .layer(socket_io_layer)
+        .layer(logging_layer)
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let mut addr: String = "0.0.0.0:".into();
+    addr.push_str(&port);
+
+    let listener = tokio::net::TcpListener::bind(addr.clone()).await.unwrap();
+    info!("Server up on {addr}");
     axum::serve(listener, app).await.unwrap();
 }
 
--- api/src/state.rs
+++ api/src/state.rs
@@ -1,4 +1,5 @@
-use sea_orm::{ConnectionTrait, Database, DatabaseConnection, DbErr, Schema};
+use log::{debug, info, LevelFilter};
+use sea_orm::{ConnectionTrait, ConnectOptions, Database, DatabaseConnection, DbErr, Schema};
 use socketioxide::SocketIo;
 use crate::models::{poll, poll_answer, poll_option};
 
@@ -9,19 +10,29 @@ pub struct AppState {
 }
 
 impl AppState {
-    pub(crate) async fn new(io: SocketIo) -> Result<Self, DbErr> {
-        let db = Database::connect("sqlite::memory:").await?;
+    pub(crate) async fn new(io: SocketIo, db_uri: &str, run_migrations: bool) -> Result<Self, DbErr> {
+        let mut opt = ConnectOptions::new(db_uri);
+        opt.sqlx_logging_level(LevelFilter::Debug);
 
-        let builder = db.get_database_backend();
-        let schema = Schema::new(builder);
+        let db = Database::connect(opt).await?;
+        info!("Database Connected!");
 
-        let statement1 = builder.build(&schema.create_table_from_entity(poll::Entity));
-        let statement2 = builder.build(&schema.create_table_from_entity(poll_option::Entity));
-        let statement3 = builder.build(&schema.create_table_from_entity(poll_answer::Entity));
+        if run_migrations {
+            info!("Running Migrations");
+            let builder = db.get_database_backend();
+            let schema = Schema::new(builder);
 
-        db.execute(statement1).await?;
-        db.execute(statement2).await?;
-        db.execute(statement3).await?;
+            let statement1 = builder.build(&schema.create_table_from_entity(poll::Entity));
+            let statement2 = builder.build(&schema.create_table_from_entity(poll_option::Entity));
+            let statement3 = builder.build(&schema.create_table_from_entity(poll_answer::Entity));
+
+            db.execute(statement1).await?;
+            db.execute(statement2).await?;
+            db.execute(statement3).await?;
+            debug!("Migrations done");
+        } else {
+            info!("Skipping Migrations");
+        }
 
         Ok(AppState { db, io })
     }
