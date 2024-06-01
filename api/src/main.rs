bb966404b369b1c289fc89b8ab7d2ae0dbe9b8d5
Méndez <47764656+El-Mendez@users.noreply.github.com>
2024-06-01 15:02:01 -0600

    finish backend

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,8 +2,9 @@ mod models;
 mod state;
 mod api;
 
-use axum::{Router, routing::get};
+use axum::Router;
 use tower_http::cors::{CorsLayer, Any};
+use tower_http::services::{ServeDir, ServeFile};
 use socketioxide::SocketIo;
 use crate::api::api_routes;
 use crate::state::AppState;
@@ -13,6 +14,8 @@ async fn main() {
     tracing_subscriber::fmt::init();
     let (layer, io) = SocketIo::new_layer();
     let state = AppState::new(io).await.expect("could not connect to db");
+    let serve_static_files = ServeDir::new("static")
+        .not_found_service(ServeFile::new("static/index.html"));
 
     let cors = CorsLayer::new()
         .allow_origin(Any)
@@ -21,7 +24,7 @@ async fn main() {
 
     let app = Router::new()
         .nest_service("/api", api_routes(state))
-        .route("/", get(root))
+        .fallback_service(serve_static_files)
         .layer(layer)
         .layer(cors);
 
@@ -29,8 +32,4 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-
-async fn root() -> &'static str {
-    "Hello, World!"
-}
 
