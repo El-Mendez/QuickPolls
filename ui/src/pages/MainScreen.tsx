2b1dc8ef738349cbbd75640e2ee10564f2073ca9
Méndez <47764656+El-Mendez@users.noreply.github.com>
2024-06-11 22:17:24 -0600

    changed text

--- ui/src/pages/MainScreen.tsx
+++ ui/src/pages/MainScreen.tsx
@@ -16,7 +16,7 @@ const MainScreen = () => {
       <Typography variant="h1">Crea una encuesta</Typography>
       <Link to="/crear">
         <Button>
-          <Typography variant="h2">WOW</Typography>
+          <Typography variant="h2">WooooW</Typography>
         </Button>
       </Link>
     </Box>
